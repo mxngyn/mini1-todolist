00a2c4ea56c0c572b8505a0832ccd9ca7352220a
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-02-02 16:48:20 -0700

    Add styling to remove

--- app/List.js
+++ app/List.js
@@ -4,7 +4,15 @@ var List = React.createClass({
   render: function(){
     var listItems = this.props.items.map(function(item, index){
       return (
-        <li key={index}> <span className="glyphicon glyphicon-remove" onClick={this.props.remove.bind(null, index)}></span>{item} </li>
+        <li key={index} className="list-group-item">
+          <span
+            className="remove-item glyphicon glyphicon-remove"
+            onClick={this.props.remove.bind(null, index)}>
+          </span>
+          <span className="todo-item">
+            {item}
+          </span>
+        </li>
       )
     }.bind(this));
     return (
