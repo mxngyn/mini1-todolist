f15a79fdb4df6873df4702e4baf4b8ca28d2a532
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-02-03 15:15:39 -0700

    Migrate styles to component

--- app/List.js
+++ app/List.js
@@ -2,21 +2,45 @@ var React = require('react');
 
 var List = React.createClass({
   render: function(){
+    var styles = {
+      uList: {
+        paddingLeft: 0,
+        listStyleType: "none"
+      },
+      listGroup: {
+        margin: '5px 0',
+        borderRadius: 5
+      },
+      removeItem: {
+        fontSize: 20,
+        float: "left",
+        position: "absolute",
+        top: 12,
+        left: 6,
+        cursor: "pointer",
+        color: "rgb(222, 79, 79)"
+      },
+      todoItem: {
+        paddingLeft: 20,
+        fontSize: 17
+      }
+    };
     var listItems = this.props.items.map(function(item, index){
       return (
-        <li key={index} className="list-group-item">
+        <li key={index} className="list-group-item" style={styles.listGroup}>
           <span
-            className="remove-item glyphicon glyphicon-remove"
+            className="glyphicon glyphicon-remove"
+            style={styles.removeItem}
             onClick={this.props.remove.bind(null, index)}>
           </span>
-          <span className="todo-item">
+          <span style={styles.todoItem}>
             {item}
           </span>
         </li>
       )
     }.bind(this));
     return (
-      <ul>
+      <ul style={styles.uList}>
         {listItems}
       </ul>
     )
