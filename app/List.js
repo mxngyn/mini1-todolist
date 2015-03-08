9ac94169bf0918f3700cc51c2cc2145e207ac122
Ryan Florence <rpflorence@gmail.com>
2015-03-08 15:01:06 -0600

    make remove button keyboard accessible

--- app/List.js
+++ app/List.js
@@ -18,7 +18,10 @@ var List = React.createClass({
         top: 12,
         left: 6,
         cursor: "pointer",
-        color: "rgb(222, 79, 79)"
+        color: "rgb(222, 79, 79)",
+        border: "none",
+        background: "none",
+        padding: 0
       },
       todoItem: {
         paddingLeft: 20,
@@ -28,11 +31,11 @@ var List = React.createClass({
     var listItems = this.props.items.map(function(item, index){
       return (
         <li key={index} className="list-group-item" style={styles.listGroup}>
-          <span
+          <button
             className="glyphicon glyphicon-remove"
             style={styles.removeItem}
-            onClick={this.props.remove.bind(null, index)}>
-          </span>
+            onClick={this.props.remove.bind(null, index)}
+          />
           <span style={styles.todoItem}>
             {item}
           </span>
